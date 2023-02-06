92ce28ff1a75be3ca7b13b2d0bde64522b367532
Jogi Dhimas Anditya <jogidhimas51@gmail.com>
2023-02-06 13:55:56 +0700

    4th-commit

--- app.js
+++ app.js
@@ -2,5 +2,18 @@ let stars = document.getElementById('stars');
 let moon = document.getElementById('moon');
 let mountainBehind = document.getElementById('mountains-behind');
 let text = document.getElementById('text');
-let btn = document.getElementById('id');
-let mountainFront = document.getElementById('mountains-front')
+let btn = document.getElementById('btn');
+let mountainFront = document.getElementById('mountains-front');
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    stars.style.left = value * 0.25 + 'px';
+    moon.style.top = value * 1.05 + 'px';
+    mountainBehind.style.top = value * 0.5 + 'px';
+    mountainFront.style.top = value * 0 + 'px';
+    text.style.marginRight = value * 4 + 'px';
+    text.style.marginTop = value * 1.5 + 'px';
+    btn.style.marginTop = value * 1.5 + 'px';
+    header.style.top = value * 0.5 + 'px';
+})
