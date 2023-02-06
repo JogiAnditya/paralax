656769f68c7b57985b896178038934a6c06589e0
Jogi Dhimas Anditya <jogidhimas51@gmail.com>
2023-02-06 13:43:39 +0700

    3rd-commit

---
+++ app.js
@@ -0,0 +1,6 @@
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountainBehind = document.getElementById('mountains-behind');
+let text = document.getElementById('text');
+let btn = document.getElementById('id');
+let mountainFront = document.getElementById('mountains-front')
